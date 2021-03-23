516d1a5a5e1d63699ef3e6072210ab6bc266a523
Vladislav Tischenko <49063810+ColaCat27@users.noreply.github.com>
2021-03-23 18:52:54 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    TOKEN: '1607007415:AAH585TpqHPt04cDvIm4C1qKhuWIeTjHewg',
-    DB_URL: 'mongodb+srv://colacat:sMqHVlIICvEleBln@cluster0.igcby.mongodb.net/coffee'
+    TOKEN: 'Токен бота',
+    DB_URL: 'URL для соединения с БД'
 }
